7a7ff60506e5a0d065ef3e17ac20106b33ad9341
Hamit Taylan YUCE <h.taylanyuce@gmail.com>
2018-07-30 22:37:27 +0300

    gps is added

--- app/src/main/java/com/example/folio1040/yourcityweather/MainActivity.java
+++ app/src/main/java/com/example/folio1040/yourcityweather/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.folio1040.yourcityweather;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -17,15 +29,78 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mEntries;
-    private final String BASE_URL= "https://samples.openweathermap.org/data/2.5/forecast?lat=41&lon=28&appid=";
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private double userLocationAltitude;
+    private double userLocationLongitude;
+    private final String BASE_URL= "https://api.openweathermap.org/data/2.5/forecast?lat="+userLocationAltitude+"&lon="+userLocationLongitude+"&appid=";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        final TextView textView1 = (TextView) findViewById(R.id.alt);
+//        final TextView textView2 = (TextView) findViewById(R.id.lon);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                userLocationAltitude = location.getLatitude();
+                userLocationLongitude = location.getLongitude();
+//                textView1.setText("altitude"+Double.toString(userLocationAltitude));
+//                textView2.setText(Double.toString(userLocationLongitude));
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION )!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+        }
+        else
+        {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+        }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION )!= PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            }
+
+        }
+
+    }
+
     // Fetch method
     private void fetch(RequestQueue requestQueue) {
         JsonArrayRequest request = new JsonArrayRequest(BASE_URL + APIs.API_KEY,
@@ -54,4 +129,5 @@ public class MainActivity extends AppCompatActivity {
         mEntries = new ArrayList<>();
         requestQueue.add(request);
     }
+
 }
